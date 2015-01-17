c3c44ae53cc0bd70e6dfa78197caf97eb4ae6647
f <fka@fatihak.in>
2015-01-17 03:27:32 +0200

    Initial release

--- source/diet_typescript.d
+++ source/diet_typescript.d
@@ -1,11 +1,13 @@
 module diet_typescript;
 
+auto partition = 0;
+
 version (Have_vibe_d)
 {
     static this()
     {
         import vibe.templ.diet, std.functional;
-        registerDietTextFilter("typescript", &memoize!filterCoffee);
+        registerDietTextFilter("typescript", &memoize!filterTypeScript);
     }
 }
 
@@ -13,26 +15,32 @@ string filterTypeScript(string script, size_t indent)
 in { assert(indent > 0); }
 body
 {
+    partition++;
     version (Have_vibe_d)
     {
         import vibe.core.log;
         logDebug("compiling typescript");
     }
-    import std.array, std.process;
+    import std.array, std.process, std.file, std.random, std.conv;
 
-    auto pipes = pipeShell("coffee -cs", Redirect.stdin | Redirect.stdout);
-    scope (exit) wait(pipes.pid);
-    pipes.stdin.rawWrite(script);
-    pipes.stdin.close();
+    string inputFile = "INLINE-TYPESCRIPT-" ~ to!string(partition) ~ ".ts";
+    string outputFile = "/tmp/tsc-diet-compile-" ~ to!string(partition) ~ ".js";
+    write(inputFile, script);
+    auto pipes = pipeShell("tsc " ~ inputFile ~ " --out " ~ outputFile, Redirect.stderrToStdout | Redirect.stdout);
+    scope(exit) wait(pipes.pid);
+    foreach (line; pipes.stdout.byLine) logWarn(line.idup);
 
-    string indent_string = "\n";
+    string[] compiled = readText(outputFile).split("\n");
+    remove(outputFile);
+    remove(inputFile);
+
+    auto indent_string = "\n";
     while (indent-- > 0) indent_string ~= '\t';
     auto res = appender!string();
     res ~= indent_string[0 .. $-1]~"<script type=\"text/javascript\">";
     res ~= indent_string~"//<![CDATA[";
-    foreach (line; pipes.stdout.byLine()) {
-        res ~= indent_string;
-        res ~= line;
+    foreach (line; compiled) {
+        res ~= indent_string~line;
     }
     res ~= indent_string~"//]]>";
     res ~= indent_string[0 .. $-1]~"</script>";
